32381054fd3baf4ee8ef01c566a49d373c5c0d70
webDeveloper42 <rfrias361@gmail.com>
2026-03-20 17:03:39 -0400

    Fix: publish page loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter r>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
